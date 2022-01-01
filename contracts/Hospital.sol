d40932b23720c3beb62d89edee7fb8e79c6d69ae
Abdul Moiz <moiza0799@gmail.com>
2022-01-01 19:54:46 +0500

    Add Update View function added in hospital contract

--- contracts/Hospital.sol
+++ contracts/Hospital.sol
@@ -13,10 +13,74 @@ contract Hospital {
         string country;
         string phoneNumber;
     }
+    uint hospitalCount = 0;
 
     mapping(uint => structHospital) public Hospitals;
 
 
-    constructor () {
+    constructor ()
+    {
+        CreateHospital("name","location","city","state","postalcode","country","phoneNumber");
+        CreateHospital("name1","location1","city1","state1","postalcode1","country1","phoneNumber1");
+        CreateHospital("name2","location1","city1","state1","postalcode1","country1","phoneNumber1");
+        CreateHospital("name3","location1","city1","state1","postalcode1","country1","phoneNumber1");
+
     }
+    function getCount() public view returns(uint)
+    {
+        return hospitalCount;
+    }
+    function CreateHospital(string memory _name, string memory _location, string memory _city, string memory _state, 
+                            string memory _postalCode, string memory _country, string memory _phoneNumber)  public  returns (bool)
+    {
+       if(!checkHospital(_name))
+       {
+           hospitalCount ++;
+           Hospitals[hospitalCount] = structHospital(hospitalCount, _name, _location, _city, _state,_postalCode, _country, _phoneNumber);
+           return true;
+       }
+        else {
+            return false; // Hospital with the same name already exist
+        }
+    }
+    function updateHospital(uint _id, string memory _name, string memory _location, string memory _city, string memory _state, 
+                            string memory _postalCode, string memory _country, string memory _phoneNumber)  public  returns (bool)
+    {
+         for(uint i = 0; i < hospitalCount; i++){
+
+            if(keccak256(abi.encodePacked(Hospitals[i].id)) == keccak256(abi.encodePacked(_id)) )
+            {
+                Hospitals[i].name = _name;
+                Hospitals[i].location = _location;
+                Hospitals[i].city = _city;
+                Hospitals[i].state = _state;
+                Hospitals[i].postalCode = _postalCode;
+                Hospitals[i].country = _country;
+                Hospitals[i].phoneNumber = _phoneNumber;
+                return true; // record updated successfully
+            }
+        }
+        return false; // invalid hospital name
+    }
+    function checkHospital(string memory _name) public view  returns (bool)
+    {
+        for(uint i = 0; i < hospitalCount; i++){
+
+            if(keccak256(abi.encodePacked(Hospitals[i].name)) == keccak256(abi.encodePacked(_name)) )
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    function getAll() public view returns (structHospital[] memory)
+    {
+        structHospital[] memory hospitalList = new structHospital[](hospitalCount+1);
+        for (uint i = 0; i <= hospitalCount; i++) 
+        {
+            hospitalList[i] = Hospitals[i];
+        }
+        return hospitalList;
+    }
+    
 }
