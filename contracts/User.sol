14a53a84e78c6c943b4974a705ebf5b039777514
Abdul Moiz <moiza0799@gmail.com>
2022-01-01 14:05:04 +0500

    Login function created

--- contracts/User.sol
+++ contracts/User.sol
@@ -14,9 +14,48 @@ contract User {
     }
 
     mapping(uint => structUser) public Users;
-   
+    //Holds user count 
+    uint public usersCount;
+    
     constructor () {
     }
-    
+    // user login function
+    function login(string memory _username, string memory _password)  public view returns (bool)
+    {
+        for(uint i = 0; i < usersCount; i++){
+
+            if(keccak256(abi.encodePacked(Users[i].username)) == keccak256(abi.encodePacked(_username)) && 
+               keccak256(abi.encodePacked(Users[i].password)) == keccak256(abi.encodePacked(_password)) )
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function createUser(string memory _username, string memory _password) public returns (bool)
+    {
+        if(!checkUsername(_username))
+        {
+            usersCount ++;
+            Users[usersCount] = structUser(usersCount,1,"Abdul Moiz",_username,_password, true, 0);
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+    }
+    function checkUsername(string memory _username) private view  returns (bool)
+    {
+        for(uint i = 0; i < usersCount; i++){
+
+            if(keccak256(abi.encodePacked(Users[i].username)) == keccak256(abi.encodePacked(_username)) )
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 
 }
