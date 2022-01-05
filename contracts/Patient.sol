7ab2cb3c5ea581968a479875ee4b640d1b67bb39
Abdul Moiz <moiza0799@gmail.com>
2022-01-05 15:00:56 +0500

    Add and getAll function added in patient contract

--- contracts/Patient.sol
+++ contracts/Patient.sol
@@ -13,11 +13,48 @@ contract Patient {
         string email;
         string status;
         string gender;
-        uint CommonMedialConditionId;
+       // commonMedicalConditions MedicalConditions;
     }
+    struct commonMedicalConditions{
+        bool AlcoholDependence;
+        bool DurgDependence;
+        bool SeizureCerebral;
+        bool SeizureAlcoholRelated;
+        bool HeatDisease;
+        bool Blackout;
+        bool Stroke;
+        bool VisualImpairment;
+        bool VisualAcuityImpairment;
+        bool VisualFieldImpairment;
+        bool Diabetes;
+        bool MentalOrEmotionalIllness;
+        bool DementiaOrAlzheimer;
+        bool SleepApnea;
+        bool Narcolesy;
+        bool MotorFucntionAbilityImpaired;
+        bool Other;
+    }
+    uint patientCount =0;
     mapping(uint => structPatient) public Patients;
 
     constructor () {
-
+        AddPatient("AbdulMoiz", "08/05/2025", "42401123123123", 1, "923142313111", "abdulmoiz@gmail.com", "healthy", "male");
+    }
+     function AddPatient( string memory _fullname, string memory _dateOfBirth, string memory _cnic, 
+                        uint _hospitalId, string memory _phoneNo, string memory _email, string memory _status, string memory _gender) public returns (bool)
+    {
+            Patients[patientCount] = structPatient( patientCount,_fullname, _dateOfBirth,  _cnic,  _hospitalId, _phoneNo, _email,  _status, _gender); 
+            patientCount ++;
+            return true;
+      
+    }
+    function getAll() public view returns (structPatient[] memory)
+    {
+        structPatient[] memory hospitalList = new structPatient[](patientCount+1);
+        for (uint i = 0; i <patientCount; i++) 
+        {
+            hospitalList[i] = Patients[i];
+        }
+        return hospitalList;
     }
 }
